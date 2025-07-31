374a7326fb3c296e407cd138ca4c856e78540bdd
krewsona <ajkrewson@gmail.com>
2025-07-31 15:33:32 -0500

    added PlayerTeam repository

---
+++ src/main/java/com/unit_2_final_Allison_K/repository/PlayerTeamRepository.java
@@ -0,0 +1,7 @@
+package com.unit_2_final_Allison_K.repository;
+
+import com.unit_2_final_Allison_K.model.PlayerTeam;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerTeamRepository extends JpaRepository<PlayerTeam, Long> {
+}
