987f9c7f3ef293d8cf862d2995c5b7655c4b24fa
krewsona <ajkrewson@gmail.com>
2025-07-31 15:25:56 -0500

    updated playerteam class - "bridge" table

--- src/main/java/com/unit_2_final_Allison_K/model/PlayerTeam.java
+++ src/main/java/com/unit_2_final_Allison_K/model/PlayerTeam.java
@@ -1,4 +1,23 @@
 package com.unit_2_final_Allison_K.model;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class PlayerTeam {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id; // unique identifier for the player-team relationship
+
+    @ManyToOne
+    @JoinColumn (name = "player_id")
+    private Player player; // reference to the Player entity
+
+    @ManyToOne
+    @JoinColumn ( name = "team_id")
+    private Team team; // reference to the Team entity
+
 }
