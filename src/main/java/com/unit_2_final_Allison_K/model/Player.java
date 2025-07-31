784ac6f0073841683446562618849364ff14849b
krewsona <ajkrewson@gmail.com>
2025-07-31 14:31:06 -0500

    added Player fields

--- src/main/java/com/unit_2_final_Allison_K/model/Player.java
+++ src/main/java/com/unit_2_final_Allison_K/model/Player.java
@@ -1,4 +1,18 @@
 package com.unit_2_final_Allison_K.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
 public class Player {
+    @Getter
+    @Setter
+    @Id //primary key
+    @GeneratedValue ( strategy = jakarta.persistence.GenerationType.IDENTITY) //id auto-generation
+    private Long id; //player's unique identifier
+    private String name;
+    private String email;
+
 }
