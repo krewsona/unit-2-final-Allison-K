c2994f00859702a345e34b297f92679daba1d1b4
krewsona <ajkrewson@gmail.com>
2025-07-31 13:55:23 -0500

    Initial commit

---
+++ src/main/java/com/unit_2_final_Allison_K/Unit2FinalAllisonKApplication.java
@@ -0,0 +1,13 @@
+package com.unit_2_final_Allison_K;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit2FinalAllisonKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit2FinalAllisonKApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unit_2_final_Allison_K/Unit2FinalAllisonKApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unit_2_final_Allison_K;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit2FinalAllisonKApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
