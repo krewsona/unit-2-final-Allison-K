f82f79801661c88a6d9b52d317d8082752b18c38
krewsona <ajkrewson@gmail.com>
2025-07-31 15:09:43 -0500

    created Team repository

---
+++ src/main/java/com/unit_2_final_Allison_K/repository/TeamRepository.java
@@ -0,0 +1,7 @@
+package com.unit_2_final_Allison_K.repository;
+import com.unit_2_final_Allison_K.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+}
