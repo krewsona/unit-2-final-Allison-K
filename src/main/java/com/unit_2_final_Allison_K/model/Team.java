b806d0f87da22ea85ed773f04d6b7ca2eb5a0587
krewsona <ajkrewson@gmail.com>
2025-07-31 15:07:06 -0500

    added team fields

--- src/main/java/com/unit_2_final_Allison_K/model/Team.java
+++ src/main/java/com/unit_2_final_Allison_K/model/Team.java
@@ -1,4 +1,17 @@
 package com.unit_2_final_Allison_K.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class Team {
+
+    @Id
+    @GeneratedValue (strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id; // team's unique identifier
+    private String name; // team's name
 }
